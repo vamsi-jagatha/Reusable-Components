871ae078ab89f1ca561e22b53991d0d7efd37d92
vamsi jagatha <vamsijagatha31@gmail.com>
2026-03-01 04:00:18 +0530

    Changes made in the Test.jsx file

--- src/pages/test/Testing.jsx
+++ src/pages/test/Testing.jsx
@@ -1,11 +1,15 @@
 import Sidebar from "../../components/sidebars/Sidebar-1";
 import { Home, Users, Settings } from "lucide-react";
 import Contact from "../../sections/contact/Contact-1";
+import ShimmerText1 from "../../components/ShimmerEffect/ShimmerText1";
+import Loader from "../Page-Loaders/Loader-1";
 
 const Testing = () => {
   return (
     <>
-      <Contact />
+      {/* <Contact /> */}
+      {/* <ShimmerText1 className="text-7xl">Vamsi</ShimmerText1> */}
+      <Loader />
     </>
   );
 };
